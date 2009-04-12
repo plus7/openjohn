96a673445eab304c7747c919294a7a1e78bbf3d8
NOSE Takafumi <ahya365@gmail.com>
2009-04-13 07:55:02 +0900

    add threadtabbar.*

---
+++ src/threadtabbar.cpp
@@ -0,0 +1,5 @@
+#include "threadtabbar.h"
+
+ThreadTabBar::ThreadTabBar()
+{
+}
---
+++ src/threadtabbar.h
@@ -0,0 +1,13 @@
+#ifndef THREADTABBAR_H
+#define THREADTABBAR_H
+
+#include <QTabBar>
+
+class ThreadTabBar : public QTabBar
+{
+    Q_OBJECT
+public:
+    ThreadTabBar();
+};
+
+#endif // THREADTABBAR_H
